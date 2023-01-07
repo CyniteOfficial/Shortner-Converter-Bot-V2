6c9586052aed31793611ab1f60d17d37386c723c
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2023-01-07 10:11:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "12124605"))
-API_HASH = os.environ.get("API_HASH", "5cf3577d85fd02286535ec2296934287")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5831968073:AAE5CP4gRFpCaFwH8SOLa09Du0vrJt0qWHo")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("1883570185")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "Your Api Id"))
+API_HASH = os.environ.get("API_HASH", "Your Api Hash")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cybuteheheuxu")
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://strange89:strange1234@cluster0.68luwly.mongodb.net/?retryWrites=true&w=majority") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "1883570185")) 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
+DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(1883570185)
+ADMINS.append(Id Owned Id)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001694641460")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "shortnerfly") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
