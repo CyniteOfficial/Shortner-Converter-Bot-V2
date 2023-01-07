d2c05a45cb0283427094ca77ba2a421fb5f5b7bb
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2023-01-07 09:48:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/hintpirox/conq.git /conq
-cd /conq
+git clone https://github.com/Cyniteofficial/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
+cd /Shortner-Converter-Bot-V2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
